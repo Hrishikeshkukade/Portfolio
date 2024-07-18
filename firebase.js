0e8234b47cc698b564958cae470695457fb849a0
Hrishikeshkukade <130929175+Hrishikeshkukade@users.noreply.github.com>
2024-07-18 21:16:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBRxuznw3K503oAopbhprRB6X6zI3ggcyw",
-    authDomain: "fir-rtc-4768d.firebaseapp.com",
-    projectId: "fir-rtc-4768d",
-    storageBucket: "fir-rtc-4768d.appspot.com",
-    messagingSenderId: "247638571490",
-    appId: "1:247638571490:web:0d124c0d86272cd42961bb"
+    apiKey: "YOUR API KEY",
+    authDomain: "---",
+    projectId: "---",
+    storageBucket: "---",
+    messagingSenderId: "--",
+    appId: "---"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
